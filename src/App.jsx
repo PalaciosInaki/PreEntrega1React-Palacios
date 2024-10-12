57f65521cea9ca8307acdf28f3953067bd717e80
Inaki Palacios <palaciosinia@gmail.com>
2024-10-12 16:11:24 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { CarritoProvider } from './context/CarritoContext'
 import Cart from './components/Cart/Cart'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Checkout from './components/Checkout/Checkout'
 
 
 
@@ -23,6 +24,7 @@ const App = () => {
             <Route path='/categoria/:idCategoria' element={<ItemListContainer />} />
             <Route path='/item/:idItem' element={<ItemDetailContainer />} />
             <Route path='/cart' element={<Cart/>}/>
+            <Route path='/checkout' element={<Checkout/>}/>
           </Routes>
         </CarritoProvider>
         <ToastContainer />

da9683b2a269b7b750f107159d3da600a8416b99
Inaki Palacios <palaciosinia@gmail.com>
2024-10-12 00:03:27 -0300

    .

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { getUnProd } from '../../asynmock'
+/* import { getUnProd } from '../../asynmock' */
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
+import { db } from '../../services/config'
+import { getDoc, doc } from 'firebase/firestore'
+
 
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState(null)
 
     const {idItem} = useParams()
 
-    useEffect(() =>{
+    useEffect(()=>{
+      const nuevoDoc = doc(db,"productos", idItem)
+
+      getDoc(nuevoDoc)
+      .then(res =>{
+        const data= res.data();
+        const nuevoProducto = {id: res.id, ...data}
+        setProducto(nuevoProducto)
+      })
+      .catch(error => console.log(error))
+    }, [idItem])
+
+    /* useEffect(() =>{
         getUnProd(idItem)
            .then (respuesta => setProducto(respuesta))
-    }, [idItem])
+    }, [idItem]) */
 
 
   return (
